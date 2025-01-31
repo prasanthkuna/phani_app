444a8ad47a6212c15b40bb8e41e5ca7da53c707e
Prasanth Kuna <prasanth.k@stackular.com>
2025-02-01 00:15:46 +0530

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@lib', replacement: '/src/lib' }
+    ]
+  },
   server: {
     host: true,
     port: 8081,
