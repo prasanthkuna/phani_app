a8994a0584c2a39df787231685c3953bdec1c92e
Prasanth Kuna <prasanth.k@stackular.com>
2025-02-01 00:26:52 +0530

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,14 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    port: 8081,
-    strictPort: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true
-      }
-    }
+    host: '0.0.0.0',
+    port: 8081
   }
 })
