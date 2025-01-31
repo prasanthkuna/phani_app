5b33baa314f287549c7308ceeedea4cc03933876
Prasanth Kuna <prasanth.k@stackular.com>
2025-02-01 00:21:19 +0530

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [
-      { find: '@', replacement: '/src' },
-      { find: '@lib', replacement: '/src/lib' }
-    ]
-  },
   server: {
     host: true,
     port: 8081,
@@ -20,15 +14,5 @@ export default defineConfig({
         changeOrigin: true
       }
     }
-  },
-  preview: {
-    port: 8081,
-    strictPort: true,
-    host: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    chunkSizeWarningLimit: 1000
   }
 })
